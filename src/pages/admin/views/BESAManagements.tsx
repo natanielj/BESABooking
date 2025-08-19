3c2bd2ee886d57fdb051995ac73a602a9f16bed6
Arely Rosendes <arosende@ucsc.edu>
2025-08-19 16:25:59 -0700

    updates with firebase again

--- src/pages/admin/views/BESAManagements.tsx
+++ src/pages/admin/views/BESAManagements.tsx
@@ -3,7 +3,9 @@ import { useState, useEffect } from 'react';
 import { X, User, Save } from 'lucide-react';
 import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 import { db } from '/Users/arely/BESABooking/BESABooking/src/firebase.ts';
-// import { mockBesas } from '../../../../data/mockData.ts';
+
+{/* Update tour #s assigned based on assigned in dashboard  */}
+{/* Click on BESA name and see all tours scheduled */}
 
 type BesaType = {
   id: string;          
