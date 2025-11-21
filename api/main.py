66794f1d79dc05f4b89a80da407fea16f3b3c395
Nataniel Jayaseelan <njaysee@ucsc.edu>
2025-11-21 14:27:04 -0800

    url changes

--- api/main.py
+++ api/main.py
@@ -23,6 +23,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://besa-booking-git-backendv4-be-student-ambassadors-projects.vercel.app/", # preview environment deploy
+    "https://besa-booking.vercel.app/"
 ]
 
 app.add_middleware(
